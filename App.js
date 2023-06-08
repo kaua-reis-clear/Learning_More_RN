dde905a8e91dc9a81cf817f3fd02e383411becbf
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-07 21:18:42 -0300

    feat: using slider

--- App.js
+++ App.js
@@ -1,45 +1,31 @@
 import React, {Component} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 
-import {Picker} from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 
-export default class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pizza: 0,
-      pizzas: [
-        {key: 1, nome: 'Strogonoff', valor: 35.9},
-        {key: 2, nome: 'Calabresa', valor: 59},
-        {key: 3, nome: 'Quatro queijos', valor: 37},
-        {key: 4, nome: 'Brigadeiro', valor: 25.7},
-        {key: 5, nome: 'Portuguesa', valor: 70},
-      ],
+      valor: 0,
     };
   }
 
   render() {
-    let pizzasItem = this.state.pizzas.map((v, k) => {
-      return <Picker.Item key={k} value={k} label={v.nome} />;
-    });
-
     return (
       <View style={styles.container}>
-        <Text style={styles.logo}>Menu Pizza</Text>
-
-        <Picker
-          selectedValue={this.state.pizza}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({pizza: itemValue})
-          }>
-          {pizzasItem}
-        </Picker>
-
-        <Text style={styles.pizzas}>
-          Voce escolheu: {this.state.pizzas[this.state.pizza].nome}
-        </Text>
-        <Text style={styles.pizzas}>
-          R$: {this.state.pizzas[this.state.pizza].valor.toFixed(2)}
+        <Slider
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={valorSelecionado =>
+            this.setState({valor: valorSelecionado})
+          }
+          value={this.state.valor}
+          minimumTrackTintColor="#0F0"
+          maximumTrackTintColor="#F00"
+        />
+        <Text style={{textAlign: 'center', fontSize: 30}}>
+          Você tem {this.state.valor.toFixed(1)} kg
         </Text>
       </View>
     );
@@ -49,16 +35,8 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
-  },
-  logo: {
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-  pizzas: {
     marginTop: 15,
-    fontSize: 25,
-    textAlign: 'center',
   },
 });
+
+export default App;
