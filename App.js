5b695a81819765f99ef4222f5eaef20a23f7aba9
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-07 21:07:57 -0300

    feat: added picker to the project

--- App.js
+++ App.js
@@ -1,41 +1,64 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 
-import Pessoas from './src/Pessoas';
+import {Picker} from '@react-native-picker/picker';
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lista: [
-        {id: '1', nome: 'Matheus', idade: 50, email: 'matheus@matheus.com'},
-        {id: '2', nome: 'Thiago', idade: 33, email: 'thiago@thiago.com'},
-        {id: '3', nome: 'Lucas', idade: 20, email: 'lucas@lucas.com'},
-        {id: '4', nome: 'Henrique', idade: 50, email: 'Henrique@Henrique.com'},
-        {id: '5', nome: 'Thiago', idade: 33, email: 'thiago@thiago.com'},
-        {id: '6', nome: 'Lucas', idade: 20, email: 'lucas@lucas.com'},
-        {id: '7', nome: 'JOSE', idade: 33, email: 'thiago@thiago.com'},
-        {id: '8', nome: 'HENRIQUE', idade: 20, email: 'lucas@lucas.com'},
+      pizza: 0,
+      pizzas: [
+        {key: 1, nome: 'Strogonoff', valor: 35.9},
+        {key: 2, nome: 'Calabresa', valor: 59},
+        {key: 3, nome: 'Quatro queijos', valor: 37},
+        {key: 4, nome: 'Brigadeiro', valor: 25.7},
+        {key: 5, nome: 'Portuguesa', valor: 70},
       ],
     };
   }
 
   render() {
+    let pizzasItem = this.state.pizzas.map((v, k) => {
+      return <Picker.Item key={k} value={k} label={v.nome} />;
+    });
+
     return (
       <View style={styles.container}>
-        <FlatList
-          data={this.state.lista}
-          keyExtractor={item => item.id}
-          renderItem={({item}) => <Pessoas data={item} />}
-        />
+        <Text style={styles.logo}>Menu Pizza</Text>
+
+        <Picker
+          selectedValue={this.state.pizza}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({pizza: itemValue})
+          }>
+          {pizzasItem}
+        </Picker>
+
+        <Text style={styles.pizzas}>
+          Voce escolheu: {this.state.pizzas[this.state.pizza].nome}
+        </Text>
+        <Text style={styles.pizzas}>
+          R$: {this.state.pizzas[this.state.pizza].valor.toFixed(2)}
+        </Text>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
+  },
+  logo: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  pizzas: {
+    marginTop: 15,
+    fontSize: 25,
+    textAlign: 'center',
   },
 });
-
-export default App;
